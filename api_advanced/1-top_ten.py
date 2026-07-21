e519374fa1615019fc485d6d375c0d6aecaf5cc1
sam-hez <h.epodoi@alustudent.com>
2026-07-21 15:54:24 +0200

    Fix Reddit hot posts endpoint

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,14 +6,18 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the first ten hot posts in a subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "python:alu-scripting:v1.0 (by /u/sam-hez)"
+    }
+    params = {
+        "limit": 10
     }
 
     response = requests.get(
         url,
         headers=headers,
+        params=params,
         allow_redirects=False
     )
 
