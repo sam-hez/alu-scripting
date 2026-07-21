eb7616e9869c3987cc8f801de8e2a95f010ebd31
sam-hez <h.epodoi@alustudent.com>
2026-07-21 16:48:49 +0200

    fix top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""
+1-top_ten module
+
+Queries the Reddit API and prints the titles of the first 10 hot posts
+for a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    If the subreddit is invalid, prints None.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "alu-scripting:top_ten:v1.0 (by /u/yourusername)"}
+    params = {"limit": 10}
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+    for post in data:
+        print(post.get("data", {}).get("title"))
